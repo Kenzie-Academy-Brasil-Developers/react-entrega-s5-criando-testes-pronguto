788b3aa534f07d74d3fdaaff0782ed3c9fa1d9fa
Gustavo <ribeiro.gustavo95@gmail.com>
2022-01-07 09:19:10 -0300

    integration test ok

--- src/__tests__/components/search.spec.js
+++ src/__tests__/components/search.spec.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Input, Button } from "semantic-ui-react";
+import Search from "../../components/Search";
+
+const mockedHandleSearch = jest.fn();
+
+jest.mock("../../providers/CepProvider", () => {
+  return {
+    useLocateCep: () => ({
+      handleSearch: mockedHandleSearch,
+      setCepNumber: jest.fn(),
+      cepNumber: 20785210,
+    }),
+  };
+});
 
 describe("Input component", () => {
   it("shoud be able to render an input", () => {
@@ -12,5 +25,19 @@ describe("Button component", () => {
   it("Shoud be able to render a Button", () => {
     render(<Button />);
     expect(screen.getByRole("button")).toBeTruthy();
+  });
+});
+
+describe("Search component", () => {
+  it("Shoud be able to render address", async () => {
+    render(<Search />);
+
+    const cepField = screen.getByPlaceholderText("Insira o CEP");
+    const buttonElement = screen.getByRole("button");
+
+    fireEvent.change(cepField, { target: { value: "20785210" } });
+    fireEvent.click(buttonElement);
+
+    expect(mockedHandleSearch).toHaveBeenCalled();
   });
 });
